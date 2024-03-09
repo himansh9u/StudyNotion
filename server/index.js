26671d45834f9e3812fb86439c07d59558a191f7
Himanshu Kumar <himansh9u@gmail.com>
2024-03-09 21:12:46 +0530

    updating cors again

--- server/index.js
+++ server/index.js
@@ -22,7 +22,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"*",
+		origin:["https://study-notion-interface.vercel.app"],
+		methods:["POST", "GET"],
 		credentials:true,
 	})
 )
